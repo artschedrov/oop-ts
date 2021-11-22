30ac36393b21556a3bae4da0d6562597596cb1be
Art Schedrov <a.sch@tutanota.com>
2021-11-22 23:32:36 +0700

    singleton

---
+++ singleton.ts
@@ -0,0 +1,17 @@
+class Database {
+  url?: number;
+  private static instance: Database;
+  
+  constructor() {
+    if(Database.instance) {
+      return Database.instance;
+    }
+    this.url = Math.random();
+    Database.instance = this;
+  }
+}
+
+const db1 = new Database();
+const db2 = new Database();
+
+console.log(db1.url, db2.url);
